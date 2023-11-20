afe000a1853946657eb3beb73c40074f03818b7d
SebaFagalde <141086010+SebaFagalde@users.noreply.github.com>
2023-11-20 14:12:25 -0300

    se agrego local storage

--- entrega3.3/main.js
+++ entrega3.3/main.js
@@ -1,11 +1,8 @@
-
 const usuarios = [
-      { usuario: "sebastian", contrasena: "1234", saldo: 21000 },
-      { usuario: "anabelen", contrasena: "contrasena2", saldo: 31500 },
-      { usuario: "fabricio", contrasena:"tutorcoder", saldo:32000}
-    ];
-
-let usuarioActual = null;
+  { usuario: "sebastian", contrasena: "1234", saldo: 21000 },
+  { usuario: "anabelen", contrasena: "contrasena2", saldo: 31500 },
+  { usuario: "fabricio", contrasena: "tutorcoder", saldo: 32000 }
+];
 
 function buscarUsuario(usuario) {
   return usuarios.find((u) => u.usuario === usuario);
@@ -15,8 +12,9 @@ function iniciarSesion() {
   const usuario = document.getElementById("usuario").value;
   const contrasena = document.getElementById("contrasena").value;
   const usuarioEncontrado = buscarUsuario(usuario);
+
   if (usuarioEncontrado && usuarioEncontrado.contrasena === contrasena) {
-    usuarioActual = usuarioEncontrado;
+    localStorage.setItem('usuario', JSON.stringify(usuarioEncontrado));
     document.getElementById("login-container").style.display = "none";
     document.getElementById("operaciones-container").style.display = "block";
     document.getElementById("resultado-container").style.display = "block";
@@ -24,7 +22,7 @@ function iniciarSesion() {
     Swal.fire({
       title: "¡Sesión iniciada!",
       text: "Bienvenido al Banco Quintero",
-      icon: "success"
+      background: ("./assets/LogoVector.png")
     });
   } else {
     document.getElementById("resultado").textContent = "Credenciales incorrectas. Sesión cerrada.";
@@ -36,21 +34,29 @@ function iniciarSesion() {
   }
 }
 
-// function consultarSaldo() {
-//   document.getElementById("resultado").textContent = `Saldo disponible: $${usuarioActual.saldo}`;
-//   registrarTransaccion("Consulta de saldo", monto);
-// }
+function obtenerUsuarioEnSesion() {
+  const usuarioEnSesion = localStorage.getItem('usuario');
+  return usuarioEnSesion ? JSON.parse(usuarioEnSesion) : null;
+}
 
+function cerrarSesion() {
+  localStorage.removeItem('usuario');
+  window.location.href = "index.html";
+  window.close();
+}
 
 function realizarDeposito() {
   const montoInput = document.getElementById("monto");
   const monto = parseFloat(montoInput.value);
-  if (!isNaN(monto) && monto > 0) {
-        usuarioActual.saldo += monto;
-    document.getElementById("resultado").textContent = `Depósito exitoso. Saldo actual: $${usuarioActual.saldo}`;
+  const usuarioEnSesion = obtenerUsuarioEnSesion();
+
+  if (usuarioEnSesion && !isNaN(monto) && monto > 0) {
+    usuarioEnSesion.saldo += monto;
+    document.getElementById("resultado").textContent = `Depósito exitoso. Saldo actual: $${usuarioEnSesion.saldo}`;
   } else {
-        document.getElementById("resultado").textContent = "Monto inválido.";
+    document.getElementById("resultado").textContent = "Monto inválido.";
   }
+
   montoInput.value = "";
   registrarTransaccion("Depósito", monto);
 }
@@ -58,68 +64,68 @@ function realizarDeposito() {
 function realizarRetiro() {
   const montoInput = document.getElementById("monto");
   const monto = parseFloat(montoInput.value);
-    if (!isNaN(monto) && monto > 0) {
-      if (monto <= usuarioActual.saldo) {
-        usuarioActual.saldo -= monto;
-        document.getElementById("resultado").textContent = `Retiro exitoso. Saldo actual: $${usuarioActual.saldo}`;
-      } else {
-        document.getElementById("resultado").textContent = "No hay suficientes fondos en su cuenta";
-      }
+  const usuarioEnSesion = obtenerUsuarioEnSesion();
+
+  if (usuarioEnSesion && !isNaN(monto) && monto > 0) {
+    if (monto <= usuarioEnSesion.saldo) {
+      usuarioEnSesion.saldo -= monto;
+      document.getElementById("resultado").textContent = `Retiro exitoso. Saldo actual: $${usuarioEnSesion.saldo}`;
     } else {
-      document.getElementById("resultado").textContent = "Monto inválido.Por favor ingrese un monto y vuelva a clickear la operación";
+      document.getElementById("resultado").textContent = "No hay suficientes fondos en su cuenta";
     }
-    montoInput.value = "";
-    registrarTransaccion("Retiro", monto);
+  } else {
+    document.getElementById("resultado").textContent = "Monto inválido.Por favor ingrese un monto y vuelva a clickear la operación";
   }
-  
-  function realizarTransferencia() {
-    const nombreDestinatario = document.getElementById("nombreDestinatario").value;
-    const nombreCuenta = document.getElementById("nombreCuenta").value.toUpperCase();
-    const bancoDestino = document.getElementById("bancoDestino").value.toUpperCase();
-    const numeroDeCuenta = document.getElementById("numeroDeCuenta").value;
-    const monto = parseFloat(document.getElementById("monto").value);
-  
-        if (!nombreDestinatario || !nombreCuenta || !bancoDestino || !numeroDeCuenta || isNaN(monto) || monto <= 0) {
-      document.getElementById("resultado").textContent = "Por favor, complete todos los campos correctamente.";
-      return;
-    }
-  
-    if (monto > usuarioActual.saldo) {
+
+  montoInput.value = "";
+  registrarTransaccion("Retiro", monto);
+}
+
+function realizarTransferencia() {
+  const nombreDestinatario = document.getElementById("nombreDestinatario").value;
+  const nombreCuenta = document.getElementById("nombreCuenta").value.toUpperCase();
+  const bancoDestino = document.getElementById("bancoDestino").value.toUpperCase();
+  const numeroDeCuenta = document.getElementById("numeroDeCuenta").value;
+  const monto = parseFloat(document.getElementById("monto").value);
+  const usuarioEnSesion = obtenerUsuarioEnSesion();
+
+  if (usuarioEnSesion && nombreDestinatario && nombreCuenta && bancoDestino && numeroDeCuenta && !isNaN(monto) && monto > 0) {
+    if (monto <= usuarioEnSesion.saldo) {
+      usuarioEnSesion.saldo -= monto;
+      const resultadoParrafo = document.createElement("p");
+      resultadoParrafo.textContent = `Se ha realizado una transferencia a ${nombreDestinatario}
+        por $${monto}
+        a ${nombreCuenta}
+        banco: ${bancoDestino}
+        a la cuenta Nº ${numeroDeCuenta}`;
+      const resultadoContainer = document.getElementById("resultado");
+      resultadoContainer.innerHTML = '';
+      resultadoContainer.appendChild(resultadoParrafo);
+      document.getElementById("transferencia-form").reset();
+      registrarTransaccion("Transferencia", monto);
+    } else {
       document.getElementById("resultado").textContent = "Fondos insuficientes para realizar la transferencia.";
-      return;
     }
-  
-      usuarioActual.saldo -= monto;
-  
-    const resultadoParrafo = document.createElement("p");
-    resultadoParrafo.textContent = `Se ha realizado una transferencia a ${nombreDestinatario}
-      por $${monto}
-      a ${nombreCuenta}
-      banco: ${bancoDestino}
-      a la cuenta Nº ${numeroDeCuenta}`;
-  
-    const resultadoContainer = document.getElementById("resultado");
-    resultadoContainer.innerHTML = '';
-    resultadoContainer.appendChild(resultadoParrafo);
-    document.getElementById("transferencia-form").reset();
-    registrarTransaccion("Transferencia", monto);
+  } else {
+    document.getElementById("resultado").textContent = "Por favor, complete todos los campos correctamente.";
   }
-  function registrarTransaccion(tipo, monto) {
-    const historialList = document.getElementById("historial-list");
-    const nuevaTransaccion = document.createElement("li");
-    nuevaTransaccion.textContent = `${tipo}: $${monto}`;
-    historialList.appendChild(nuevaTransaccion);
-  }
-  
-  function consultarSaldo() {
-    const saldoActual = usuarioActual.saldo;
+}
+
+function registrarTransaccion(tipo, monto) {
+  const historialList = document.getElementById("historial-list");
+  const nuevaTransaccion = document.createElement("li");
+  nuevaTransaccion.textContent = `${tipo}: $${monto}`;
+  historialList.appendChild(nuevaTransaccion);
+}
+
+function consultarSaldo() {
+  const usuarioEnSesion = obtenerUsuarioEnSesion();
+
+  if (usuarioEnSesion) {
+    const saldoActual = usuarioEnSesion.saldo;
     document.getElementById("resultado").textContent = `Consulta de saldo: $${saldoActual}`;
     registrarTransaccion("Consulta de saldo", saldoActual);
+  } else {
+    console.error("Usuario no encontrado en sesión");
   }
-  
-  
-function cerrarSesion (){
-  localStorage.removeItem('usuario');
-  window.location.href="index.html"
-   window.close();
- }
+}
