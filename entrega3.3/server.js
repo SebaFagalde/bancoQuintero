bb10e6f61d287b8eb917d22a29d57c7a925b4462
SebaFagalde <141086010+SebaFagalde@users.noreply.github.com>
2023-11-29 17:50:34 -0300

    borrados archivos innecesarios

--- entrega3.3/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-
-app.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
-});
